b908b75904683c5d1b27265c1be3ea4c7e231d8a
mbenson025 <mbenson025@gmail.com>
2022-08-05 15:26:55 -0500

    thought model. adding moment for now

--- models/Thought.js
+++ models/Thought.js
@@ -1,31 +1,23 @@
 const { Schema, model } = require('mongoose');
 
-// Schema to create a course model
-const courseSchema = new Schema(
+const thoughtSchema = new Schema(
   {
     thoughtText: {
       type: String,
       required: true,
+      minLength: 1,
+      maxLength: 280,
     },
-    inPerson: {
-      type: Boolean,
-      default: true,
-    },
-    startDate: {
+    createdAt: {
       type: Date,
       default: Date.now(),
+      //add getter method for datestamp
     },
-    endDate: {
-      type: Date,
-      // Sets a default value of 12 weeks from now
-      default: () => new Date(+new Date() + 84 * 24 * 60 * 60 * 1000),
+    username: {
+      type: String,
+      required: true,
     },
-    students: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Student',
-      },
-    ],
+    reactions: [{ reactionSchema }],
   },
   {
     toJSON: {
@@ -35,6 +27,10 @@ const courseSchema = new Schema(
   }
 );
 
-const Course = model('course', courseSchema);
+thoughtSchema.virtual('reactionCount').get(function () {
+  return `${this.reactions.length}`;
+});
 
-module.exports = Course;
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
